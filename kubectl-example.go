3a4a7a9c85cdd50da83ec4daf728f61cf5624c9f
簡士博 <james@MacBook-Pro.oa.newgarden.tw>
2019-07-15 12:08:07 +0800

    運行在GKE，可解析request.body帶入參數建立job

--- kubectl-example.go
+++ kubectl-example.go
@@ -1,56 +1,82 @@
-package p
+package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"html"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	batchv1 "k8s.io/api/batch/v1"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
 
 	// Uncomment to load all auth plugins
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
-func KubectlExample(w http.ResponseWriter, r *http.Request) {
-	kubeconfig := "./kube_config"
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+var kubeClient *kubernetes.Clientset
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		fmt.Fprintf(w, "Only support POST")
+		return
+	}
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	var msg map[string]string
+	json.Unmarshal(b, &msg)
+	// fmt.Printf("Request body: %s", msg)
+
+	job := createJob(msg)
+	// deploy a job
+	jobsClient := kubeClient.BatchV1().Jobs("default")
+	backofflimit := new(int32)
+	*backofflimit = 5
+
+	result1, err1 := jobsClient.Create(job)
+	if err != nil {
+		fmt.Println(err1)
+		panic(err1)
+	}
+	fmt.Printf("Created job %q.\n", result1)
+	fmt.Fprintf(w, "Start running a job successful !")
+}
+
+func createClient() error {
+	config, err := rest.InClusterConfig()
 	if err != nil {
 		panic(err.Error())
 	}
 
 	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+	kubeClient, err = kubernetes.NewForConfig(config)
+	return err
+}
 
-	// do something
-	pods, err := clientset.CoreV1().Pods("").List(metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
+func conertToContainerEnvVar(msg map[string]string) []apiv1.EnvVar {
+	result := []apiv1.EnvVar{}
+	for k, v := range msg {
+		result = append(result, apiv1.EnvVar{
+			Name:  k,
+			Value: v,
+		})
 	}
-	fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+	return result
+}
 
-	// deploy a job
-	jobsClient := clientset.BatchV1().Jobs("default")
-	// result1, err1 := jobsClient.List(metav1.ListOptions{})
-	// if err != nil {
-	// 	fmt.Println(err1)
-	// 	panic(err1)
-	// }
-	// fmt.Println(result1.Items[0].Spec)
-	// fmt.Println(fmt.Sprintf("%T", result1.Items[0].Spec))
+func createJob(msg map[string]string) *batchv1.Job {
 	backofflimit := new(int32)
 	*backofflimit = 5
 	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "build-firmware",
+			Name:      "build-firmware-" + msg["ORDER_NUM"],
 			Namespace: "default",
 		},
 		Spec: batchv1.JobSpec{
@@ -61,12 +87,7 @@ func KubectlExample(w http.ResponseWriter, r *http.Request) {
 						{
 							Name:  "build-firmware",
 							Image: "gcr.io/ultron-cms/helloworld",
-							Env: []apiv1.EnvVar{
-								{
-									Name:  "HELLO_MSG",
-									Value: "James !!!!!!",
-								},
-							},
+							Env:   conertToContainerEnvVar(msg),
 						},
 					},
 					RestartPolicy: "Never",
@@ -77,23 +98,14 @@ func KubectlExample(w http.ResponseWriter, r *http.Request) {
 			},
 		},
 	}
-	result1, err1 := jobsClient.Create(job)
-	if err != nil {
-		fmt.Println(err1)
-		panic(err1)
-	}
-	fmt.Printf("Created job %q.\n", result1)
+	return job
+}
 
-	var d struct {
-		Message string `json:"message"`
+func main() {
+	err := createClient()
+	if err != nil {
+		panic(err.Error())
 	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World!")
-		return
-	}
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
-		return
-	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
